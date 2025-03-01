b2ad36d58ae93ce464d24607eabdf5215e184c1e
Aziz <Fabolouse228@gmail.com>
2025-03-01 11:19:25 +0200

    added axios, tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,25 @@
+'use client';
+import { Button } from '@mui/material';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    axios.get('https://skillfactory-task.detmir.team/categories')
+      .then(response => setData(response.data))
+      .catch(error => console.error('Ошибка', error));
+  }, []);
+
+  if (!data) return <p>Загрузка...</p>;
+
   return (
-    <p>Test</p>
+    <>
+      <h1>{data}</h1>
+      <Button variant="contained" color="primary">
+        Я стилизованная кнопка
+      </Button>
+    </>
   );
 }
